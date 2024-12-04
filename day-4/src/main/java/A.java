c369d99dbed2745ce362fadd0e73f2e2caf59f0f
Nemanja <npolic2@gmail.com>
2024-12-04 16:46:42 +0100

    fix: minor changes

--- day-4/src/main/java/A.java
+++ day-4/src/main/java/A.java
@@ -1,6 +1,6 @@
 public class A {
     public static void main(String[] args) {
-        var input = InputReader.getInputByLine("day-4/src/main/resources/input.txt");
+        var input = InputReader.getInputByLine("day-4/src/main/resources/test.txt");
         for(String line : input){
             System.out.println(line);
         }
